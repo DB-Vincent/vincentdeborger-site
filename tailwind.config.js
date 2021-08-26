34f01649f3828a26e5bdf21ad19b77a2058f6082
Vincent De Borger <hello@vincentdeborger.be>
2021-08-26 12:01:55 +0200

    Fix dark mode markdown styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,41 +3,44 @@ module.exports = {
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {
-      // typography: {
-      //   DEFAULT: {
-      //     css: {
-      //       color: 'white',
-      //       h1: {
-      //         color: 'white'
-      //       },
-      //       h2: {
-      //         color: 'white'
-      //       },
-      //       h3: {
-      //         color: 'white'
-      //       },
-      //       h4: {
-      //         color: 'white'
-      //       },
-      //       blockquote: {
-      //         color: 'white'
-      //       },
-      //       th: {
-      //         color: 'white'
-      //       },
-      //       strong: {
-      //         color: 'white'
-      //       },
-      //       code: {
-      //         color: 'white'
-      //       }
-      //     },
-      //   },
-      // }
+      typography(theme) {
+        return {
+          dark: {
+            css: {
+              color: theme("colors.gray.300"),
+              '[class~="lead"]': { color: theme("colors.gray.400") },
+              a: { color: theme("colors.gray.100") },
+              strong: { color: theme("colors.gray.100") },
+              "ul > li::before": { backgroundColor: theme("colors.gray.700") },
+              hr: { borderColor: theme("colors.gray.800") },
+              blockquote: {
+                color: theme("colors.gray.100"),
+                borderLeftColor: theme("colors.gray.800"),
+              },
+              h1: { color: theme("colors.gray.100") },
+              h2: { color: theme("colors.gray.100") },
+              h3: { color: theme("colors.gray.100") },
+              h4: { color: theme("colors.gray.100") },
+              code: { color: theme("colors.gray.100") },
+              "a code": { color: theme("colors.gray.100") },
+              pre: {
+                color: theme("colors.gray.200"),
+                backgroundColor: theme("colors.gray.800"),
+              },
+              thead: {
+                color: theme("colors.gray.100"),
+                borderBottomColor: theme("colors.gray.700"),
+              },
+              "tbody tr": { borderBottomColor: theme("colors.gray.800") },
+            },
+          },
+        };
+      },
     },
   },
   variants: {
     marginBottom: ({ after }) => after(["last-of-type"]),
+    extend: { typography: ["dark"] }
   },
   corePlugins: {
     container: false
